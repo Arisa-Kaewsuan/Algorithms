dc54fa724c1cfe426812f9d78707ec9b44e2c26a
Arisa-Kaewsuan <Arisa.Kaewsuan@gmail.com>
2023-09-27 09:37:35 +0700

    add Merge_Sort code

--- Divide_&_Conquer/Binary_Search_Tree.java
+++ Divide_&_Conquer/Binary_Search_Tree.java
@@ -1,8 +1,10 @@
 
-public class Binary_Search_Tree {
+public class Binary_Search_Tree 
+{
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
+		
 
 	}
 
