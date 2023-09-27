a15e6a2c5ca92607002b6b88fc027b40dff24eaf
Arisa-Kaewsuan <Arisa.Kaewsuan@gmail.com>
2023-09-27 09:26:07 +0700

    add Fbonacci

--- Recursive/Fibonacci/Loop_Fibonacci.java
+++ Recursive/Fibonacci/Loop_Fibonacci.java
@@ -1,9 +1,22 @@
 package Fibonacci;
 
-public class Loop_Fibonacci {
+public class Loop_Fibonacci 
+{
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
+		int n = 10, firstTerm = 0, secondTerm = 1;
+	    System.out.println("Fibonacci Series till " + n + " terms:");
+
+	    for (int i = 1; i <= n; ++i) 
+	    {
+	      System.out.print(firstTerm + ", ");
+
+	      // compute the next term
+	      int nextTerm = firstTerm + secondTerm;
+	      firstTerm = secondTerm;
+	      secondTerm = nextTerm;
+	    }
 
 	}
 
