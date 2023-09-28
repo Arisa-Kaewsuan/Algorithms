2bb82ae1960a1771cc59f73a1c88384b97462acc
Arisa-Kaewsuan <Arisa.Kaewsuan@gmail.com>
2023-09-29 00:36:30 +0700

    add N-Queen Problem

---
+++ Travelling_Salesman_Problem/BackTracking_TSP/BackTracking_TSP.java
@@ -0,0 +1,10 @@
+package BackTracking_TSP;
+
+public class BackTracking_TSP {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Travelling_Salesman_Problem/Dynamic_TSP/Dynamic_TSP.java
@@ -0,0 +1,10 @@
+package Dynamic_TSP;
+
+public class Dynamic_TSP {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
